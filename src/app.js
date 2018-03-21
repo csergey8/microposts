fb5cd7ea7061944e64cec54cda398e46402eb812
csergey8 <c.sergey@protonmail.com>
2018-03-21 17:32:08 +0200

    add post and show alert

--- src/app.js
+++ src/app.js
@@ -4,8 +4,28 @@ import { ui } from './ui';
 // Get post on DOMload
 document.addEventListener('DOMContentLoaded', getPost);
 
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 function getPost() {
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPost(data)) 
+        .catch(err => console.log(err));
+}
+
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+    
+    const data = {
+        title,
+        body
+    }
+    
+    http.post('http://localhost:3000/posts', data)
+        .then(data => {
+            ui.showAlert('Post added', 'alert alert-success');
+            ui.clearFields()
+            getPost();
+        })
         .catch(err => console.log(err));
 }
